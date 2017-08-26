8f14106bc196c01ca3f382f9905bac9827fd8802
cesar8803 <hlj.cesar@gmail.com>
2017-08-26 11:22:58 -0500

    OnclickListener del CardView del RecyclerView
    OnclickListener del CardView del RecyclerView

--- PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/adapter/ListLocationVenuesAdapter.java
+++ PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/adapter/ListLocationVenuesAdapter.java
@@ -1,10 +1,14 @@
 package mx.mobilestudio.placefinder.adapter;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -40,8 +44,7 @@ public class ListLocationVenuesAdapter extends RecyclerView.Adapter{
         ((MyViewHolder)holder).name.setText(venues.get(position).getName());
         ((MyViewHolder)holder).distance.setText(venues.get(position).getLocation().getDistance().toString());
         ((MyViewHolder)holder).address.setText(venues.get(position).getLocation().getAddress());
-
-
+        ((MyViewHolder)holder).position = position;
     }
 
     @Override
@@ -50,11 +53,13 @@ public class ListLocationVenuesAdapter extends RecyclerView.Adapter{
     }
 
 
-    public class MyViewHolder extends  RecyclerView.ViewHolder{
+    public class MyViewHolder extends  RecyclerView.ViewHolder implements View.OnClickListener{
 
         public TextView name;
         public TextView distance;
         public  TextView address;
+        public CardView cardView;
+        public  int position;
 
         public MyViewHolder(View itemView) {
             super(itemView);
@@ -62,8 +67,18 @@ public class ListLocationVenuesAdapter extends RecyclerView.Adapter{
             name = (TextView) itemView.findViewById(R.id.name_text_view);
             distance = (TextView) itemView.findViewById(R.id.distance_text_view);
             address = (TextView) itemView.findViewById(R.id.format_address_text_view);
+            cardView = (CardView) itemView.findViewById(R.id.cardview);
+            cardView.setOnClickListener(this);
 
+        }
 
+        @Override
+        public void onClick(View vistafea) {
+           // LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,400);
+
+            //vistafea.setLayoutParams(layoutParams);
+
+            Toast.makeText(vistafea.getContext(), "Se dio click en "+position, Toast.LENGTH_LONG).show();
         }
     }
 
